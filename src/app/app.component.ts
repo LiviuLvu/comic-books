b02798ecc771a0ff93552e6ca7ec29d4b28719f6
Liviu Iancu <liviuiancu@gmx.com>
2017-07-22 16:16:54 +0300

    updated details page info and link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <my-comics></my-comics>
+    <footer class="footer text-center">
+      <p>View source code on <a href="https://github.com/LiviuLvu/comic-books" target="_blank">Github</a></p>
+      <p>App built by <a href="https://liviulvu.github.io" target="_blank">Liviu Iancu</a></p>
+      <p>Data provided by <a href="https://marvel.com/" target="_blank">Marvel.</a> © 2014 Marvel</p>
+    </footer>
   `
 })
 export class AppComponent { }
